6f263c3199ba9af8b07d3ec83d4fda96f78c1245
Daksh-2508 <its.dakshgoyal@gmail.com>
2025-10-12 20:26:55 +0530

    Add files via upload

---
+++ #100DaysofCode/ques101.c
@@ -0,0 +1,13 @@
+/*Q1: Print HelloWorld
+Link: https://www.hackerrank.com/challenges/hello-world-c/problem?isFullScreen=true*/
+
+#include <stdio.h>
+
+int main() {
+    char s[100]="Welcome to C Programming.";
+
+    printf("Hello, World!\n");
+    printf("%s",s);
+
+    return 0;
+}
